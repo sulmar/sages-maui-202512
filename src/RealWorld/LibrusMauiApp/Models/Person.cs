d8f1df0a6c0c3fc1f108226b3567fc01d76140c7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-16 15:39:55 +0100

    Dodanie szablonu danych DataTemplate

--- src/RealWorld/LibrusMauiApp/Models/Person.cs
+++ src/RealWorld/LibrusMauiApp/Models/Person.cs
@@ -5,9 +5,4 @@ public class Person : Base
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public byte[] Photo { get; set; }
-        
-    public override string ToString()
-    {
-        return $"{FirstName} {LastName}";
-    }
 }
