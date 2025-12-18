8d1f1f86a872d2610e4341e0459052e146c82d32
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-18 16:46:44 +0100

    Dodanie wybranego zdjecia

--- src/ZadaniaEgz/GaleriaMauiApp/Pages/GaleriaPage.xaml.cs
+++ src/ZadaniaEgz/GaleriaMauiApp/Pages/GaleriaPage.xaml.cs
@@ -6,23 +6,6 @@ namespace GaleriaMauiApp.Pages;
 
 public partial class GaleriaPage : ContentPage
 {
-    private string galleryJson = @"
-    [
-        { ""id"": 0,  ""alt"": ""Mak"",             ""filename"": ""obraz1.jpg"",  ""category"": 1, ""downloads"": 35 },
-        { ""id"": 1,  ""alt"": ""Bukiet"",          ""filename"": ""obraz2.jpg"",  ""category"": 1, ""downloads"": 43 },
-        { ""id"": 2,  ""alt"": ""Dalmatyñczyk"",    ""filename"": ""obraz3.jpg"",  ""category"": 2, ""downloads"": 2 },
-        { ""id"": 3,  ""alt"": ""Œwinka morska"",   ""filename"": ""obraz4.jpg"",  ""category"": 2, ""downloads"": 53 },
-        { ""id"": 4,  ""alt"": ""Rotwailer"",       ""filename"": ""obraz5.jpg"",  ""category"": 2, ""downloads"": 43 },
-        { ""id"": 5,  ""alt"": ""Audi"",             ""filename"": ""obraz6.jpg"",  ""category"": 3, ""downloads"": 11 },
-        { ""id"": 6,  ""alt"": ""kotki"",           ""filename"": ""obraz7.jpg"",  ""category"": 2, ""downloads"": 22 },
-        { ""id"": 7,  ""alt"": ""Ró¿a"",             ""filename"": ""obraz8.jpg"",  ""category"": 1, ""downloads"": 33 },
-        { ""id"": 8,  ""alt"": ""Œwinka morska"",   ""filename"": ""obraz9.jpg"",  ""category"": 2, ""downloads"": 123 },
-        { ""id"": 9,  ""alt"": ""Foksterier"",      ""filename"": ""obraz10.jpg"", ""category"": 2, ""downloads"": 22 },
-        { ""id"": 10, ""alt"": ""Szczeniak"",       ""filename"": ""obraz11.jpg"", ""category"": 2, ""downloads"": 12 },
-        { ""id"": 11, ""alt"": ""Garbus"",           ""filename"": ""obraz12.jpg"", ""category"": 3, ""downloads"": 321 }
-    ]
-    ";
-
     private List<Zdjecie>? wszystkieZdjecia;
     private List<Zdjecie> przefiltrowaneZdjecia = new List<Zdjecie>();
 
@@ -46,15 +29,16 @@ public partial class GaleriaPage : ContentPage
 
         // Reczne tworzenie klienta HttpClient
 
+        // Przypisanie adresu bazowego na podstawie systemu
 
         string baseUrl =
-#if ANDROID
-     "https://10.0.2.2:5000"; // 10.0.2.2 to jest adres ip emulatora android
-#elif WINDOWS
-     "https://localhost:5000";
-#else
-     "https://localhost:5000";
-#endif
+            #if ANDROID
+                 "https://10.0.2.2:5000"; // 10.0.2.2 to jest adres ip emulatora android
+            #elif WINDOWS
+                 "https://localhost:5000";
+            #else
+                 "https://localhost:5000";
+            #endif
 
 
         var handler = new HttpClientHandler
@@ -62,22 +46,16 @@ public partial class GaleriaPage : ContentPage
             ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true
         };
 
-        HttpClient client = new HttpClient(handler);
+        client = new HttpClient(handler);
         client.BaseAddress = new Uri(baseUrl);
 
-
-        try
-        {
-            wszystkieZdjecia = client.GetFromJsonAsync<List<Zdjecie>>("/api/zdjecia").Result;
-        }
-        catch (Exception ex)
-        {
-        }
-
-
+        wszystkieZdjecia = client.GetFromJsonAsync<List<Zdjecie>>("/api/zdjecia").Result;
+        
         ZastosujFiltr();
 
         ZdjeciaCollection.ItemsSource = przefiltrowaneZdjecia;
+
+        WyswietlZdjecie();
 
     }
 
@@ -122,8 +100,6 @@ public partial class GaleriaPage : ContentPage
         if (SamochodySwitch.IsToggled)
             wybraneKategorie.Add(3);
 
-
-
         foreach (Zdjecie zdjecie in wszystkieZdjecia)
         {
             if (wybraneKategorie.Contains(zdjecie.Category))
@@ -133,5 +109,33 @@ public partial class GaleriaPage : ContentPage
         }
 
         Odswiez();
+    }
+
+
+    private int index = 0;
+
+    private void WyswietlZdjecie()
+    {
+        WybraneZdjecie.Source = przefiltrowaneZdjecia[index].Filename;
+    }
+
+    private void Wstecz_Clicked(object sender, EventArgs e)
+    {        
+        if (index > 0)
+        {
+            index--;
+            WyswietlZdjecie();
+
+
+        }
+    }
+
+    private void Dalej_Clicked(object sender, EventArgs e)
+    {
+        if (index < przefiltrowaneZdjecia.Count - 1)
+        {
+            index++;
+            WyswietlZdjecie();
+        }
     }
 }
