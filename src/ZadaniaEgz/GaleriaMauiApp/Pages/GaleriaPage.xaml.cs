443001de631d4a356f9cf684eba8f64cc42d59e5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-18 15:18:45 +0100

    Dodanie przelacznika adresu

--- src/ZadaniaEgz/GaleriaMauiApp/Pages/GaleriaPage.xaml.cs
+++ src/ZadaniaEgz/GaleriaMauiApp/Pages/GaleriaPage.xaml.cs
@@ -27,16 +27,14 @@ public partial class GaleriaPage : ContentPage
     private List<Zdjecie> przefiltrowaneZdjecia = new List<Zdjecie>();
 
     private HttpClient client;
-    
-    public GaleriaPage(IHttpClientFactory factory)
+
+    public GaleriaPage()
     {
         InitializeComponent();
 
-        client = factory.CreateClient("api");
-
         ZaladujZdjecia();
     }
-    
+
     private void ZaladujZdjecia()
     {
         //JsonSerializerOptions options = new JsonSerializerOptions
@@ -48,8 +46,16 @@ public partial class GaleriaPage : ContentPage
 
         // Reczne tworzenie klienta HttpClient
 
-        /*
-       string baseUrl = "https://10.0.2.2:5000";
+
+        string baseUrl =
+#if ANDROID
+     "https://10.0.2.2:5000"; // 10.0.2.2 to jest adres ip emulatora android
+#elif WINDOWS
+     "https://localhost:5000";
+#else
+     "https://localhost:5000";
+#endif
+
 
         var handler = new HttpClientHandler
         {
@@ -58,7 +64,7 @@ public partial class GaleriaPage : ContentPage
 
         HttpClient client = new HttpClient(handler);
         client.BaseAddress = new Uri(baseUrl);
-        */
+
 
         try
         {
@@ -69,17 +75,17 @@ public partial class GaleriaPage : ContentPage
         }
 
 
-        ZastosujFiltr();        
+        ZastosujFiltr();
 
         ZdjeciaCollection.ItemsSource = przefiltrowaneZdjecia;
 
     }
 
     private void Pobierz_Clicked(object sender, EventArgs e)
-    {        
-       // Button button = (Button)sender; // rzutowanie (casting)
+    {
+        // Button button = (Button)sender; // rzutowanie (casting)
 
-        if (sender is Button button  && button.BindingContext is Zdjecie zdjecie) // bezpieczniejsze
+        if (sender is Button button && button.BindingContext is Zdjecie zdjecie) // bezpieczniejsze
         {
             zdjecie.Downloads++;
 
@@ -88,7 +94,7 @@ public partial class GaleriaPage : ContentPage
     }
 
     private void Odswiez()
-    {  
+    {
         // Wymuszenie odswiezenia UI bez INotifyPropertyChanged
         ZdjeciaCollection.ItemsSource = null;
         ZdjeciaCollection.ItemsSource = przefiltrowaneZdjecia;
@@ -107,7 +113,7 @@ public partial class GaleriaPage : ContentPage
         przefiltrowaneZdjecia.Clear();
         wybraneKategorie.Clear();
 
-        if (KwiatySwitch.IsToggled) 
+        if (KwiatySwitch.IsToggled)
             wybraneKategorie.Add(1);
 
         if (ZwierzetaSwitch.IsToggled)
@@ -116,9 +122,9 @@ public partial class GaleriaPage : ContentPage
         if (SamochodySwitch.IsToggled)
             wybraneKategorie.Add(3);
 
-       
 
-        foreach(Zdjecie zdjecie in wszystkieZdjecia)
+
+        foreach (Zdjecie zdjecie in wszystkieZdjecia)
         {
             if (wybraneKategorie.Contains(zdjecie.Category))
             {
